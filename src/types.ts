df58310eee3da3cb07f706fdaaa38f1cac74c325
caasion <103465188+caasion@users.noreply.github.com>
2025-10-03 11:11:06 -0400

    Update Plugin Types

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,6 @@
+import type { Day } from "date-fns";
+
+/* Plugin Data Types */
 export type ISODate = string; // Create date type for dates in ISO 8601 for simplification (not as heavy as a Date object)
 
 export type ActionItemID = string;
@@ -12,4 +15,39 @@ export interface PlannerState {
     actionItems: Record<ActionItemID, ActionItemMeta>;
     templates: Record<ISODate, ActionItemID[]>;
     cells: Record<ISODate, Record<ActionItemID, string /* contents */>>; // Records are much more efficient objects for look-ups
+}
+
+/* Data persistence */
+export interface PluginData {
+    version: number;
+    settings: PluginSettings;
+    planner: PlannerState;
+    calendar: CalendarCache;
+}
+
+export interface PluginSettings {
+    weekStartOn: Day;
+    autosaveDebounceMs: number;
+    weeksToRender: number;
+    remoteCalendarUrl: string;
+    refreshRemote: number;
+}
+
+export interface CalendarCache {
+    data?: string;
+}
+
+/* DEFAULT VALUES */
+export const DEFAULT_SETTINGS: PluginSettings = {
+    weekStartOn: 0,
+    autosaveDebounceMs: 200,
+    weeksToRender: 1,
+    remoteCalendarUrl: "",
+    refreshRemote: 5,
+}
+
+export const EMPTY_PLANNER: PlannerState = {
+    actionItems: {},
+    cells: {},
+    templates: {}
 }
