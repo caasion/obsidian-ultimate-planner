c16a07ddaa47901a4dd14f31eea7747a1c2ff1b3
caasion <103465188+caasion@users.noreply.github.com>
2025-10-13 11:35:54 -0400

    [chore] Remove Unused Modal

--- src/ui/ActionItemModals.ts
+++ src/ui/ActionItemModals.ts
@@ -2,33 +2,7 @@ import { Modal, Setting } from "obsidian";
 import type { App } from 'obsidian'
 import type { ActionItemMeta } from "../types";
 
-export class RenameActionItemModal extends Modal {
-
-    constructor(app: App, initial: ActionItemMeta, onSubmit: (label: string, color: string) => void) {
-        super(app);
-        
-        const { contentEl } = this;
-        let {label, color} = initial;
-        new Setting(contentEl)
-            .setName("New name")
-            .addText((t) => t.setValue(label).onChange((v) => (label = v)));
-        new Setting(contentEl)
-            .setName("New color")
-            .addColorPicker(c => c  
-                .setValue(color)
-                .onChange((v) => color = v)
-            );
-        new Setting(contentEl)
-            .addButton((b) => b.setButtonText("Save").setCta().onClick(() => { 
-                onSubmit(label, color); 
-                this.close(); }))
-            .addButton((b) => b.setButtonText("Cancel").onClick(() => this.close()));
-        }
-
-}
-
-export class ReplaceActionItemModal extends Modal {
-
+export class EditActionItemModal extends Modal {
     constructor(app: App, initial: ActionItemMeta, onSubmit: (label: string, color: string) => void) {
         super(app);
         
