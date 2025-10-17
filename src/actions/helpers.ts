15031162e7a6ea2419043e6e921166071be66beb
caasion <103465188+caasion@users.noreply.github.com>
2025-10-17 09:57:35 -0400

    [refactor + docs] Clean Up Helpers
    - removed idUsedInTemplates
    - added function documentation
    - added swapArrayItems
    - added getISODates (with function overloading)

--- src/actions/helpers.ts
+++ src/actions/helpers.ts
@@ -1,41 +1,49 @@
-import type { ISODate, ItemID } from '../types';
-/* Helper Functions */
+import type { ISODate } from '../types';
 import { addDays, eachDayOfInterval, endOfWeek, format, parseISO, startOfWeek, type Day } from 'date-fns';
 
+
+/** Returns a randomUUID() with a given prefix. */
 export function generateID(prefix: string) {
     return prefix + crypto.randomUUID();
 }
 
-// Date Helpers
+/** Formats a Date into an ISODate. */
 export function getISODate(date: Date): ISODate {
     return format(date, "yyyy-MM-dd")
 }
 
+/** Adds days to an ISODate (which cannot be done directly in date-fns). */
 export function addDaysISO(iso: ISODate, n: number): ISODate {
     return getISODate(addDays(parseISO(iso), n));
 }
 
-export function getISODatesOfWeek(anchorDate: ISODate, weeks: number, weekStartsOn: Day = 0): ISODate[][] {
-    const date = parseISO(anchorDate);
+/** Get the ISODates from and including today until the specified number. */
+export function getISODates(anchor: Date, days: number): ISODate[];
+/** Get the ISODates of the weeks from and including this week given the anchor, week number, and the day the week starts on. */
+export function getISODates(anchor: Date, weeks: number, weekStartsOn: Day): ISODate[];
+export function getISODates(anchor: Date, amount: number, weekStartsOn?: Day) {
+    if (weekStartsOn) { // If we are looking for the dates of a week
+        let dates: ISODate[] = [];
+        
+        for (let i = 0; i < amount; i++) {
+            const start = startOfWeek(addDays( anchor, i * 7 ), { weekStartsOn });
+            const end = endOfWeek(addDays( anchor, i * 7 ), { weekStartsOn });
 
-    let isoDates: ISODate[][] = [];
+            const days = eachDayOfInterval({ start, end });
 
-    for (let i = 0; i < weeks; i++) {
-        const start = startOfWeek(addDays( date, i * 7 ), { weekStartsOn });
-        const end = endOfWeek(addDays( date, i * 7 ), { weekStartsOn });
+            days.forEach(day => dates.push(getISODate(day)))
+        }
 
-        const days = eachDayOfInterval({ start, end });
+        return dates;
+    } else { // IF we just want to get the dates of a block
+        const dates = eachDayOfInterval({ start: anchor, end: addDays(anchor, amount)})
 
-        isoDates.push(days.map(day => getISODate(day)));
+        return dates.map(d => getISODate(d));
     }
-
-    return isoDates;
 }
 
-export function getLabelFromDateRange(firstDate: ISODate, lastDate: ISODate) {
-    const first = parseISO(firstDate);
-    const last = parseISO(lastDate);
-
+/** [PURE HELPER] Gets a well-formmated label given a date range. */
+export function getLabelFromDateRange(first: Date, last: Date) {
     if (first.getFullYear() === last.getFullYear()) {
         if (first.getMonth() === last.getMonth()) {
             return `${format(first, "MMM")} ${format(first, "dd")} – ${format(last, "dd")}, ${format(first, "yyyy")}`
@@ -45,16 +53,6 @@ export function getLabelFromDateRange(firstDate: ISODate, lastDate: ISODate) {
     } else {
         return `${format(first, "MMM")} ${format(first, "dd")}, ${format(first, "yyyy")} – ${format(last, "MMM")} ${format(last, "dd")}, ${format(last, "yyyy")}`
     }
-
-}
-
-// Templates helpers
-export function idUsedInTemplates(templates: Record<ISODate, ItemID[]>, rowID: ItemID): boolean {
-    // Check all template arrays
-    for (const arr of Object.values(templates)) {
-        if (arr.includes(rowID)) return true;
-    }
-    return false;
 }
 
 /** [PURE HELPER] Hash a string using SHA-1. */
@@ -64,3 +62,12 @@ export async function hashText(text: string): Promise<string> {
   return Array.from(new Uint8Array(buf)).map(b => b.toString(16).padStart(2, '0')).join('');
 }
 
+/** [PURE HELPER] Takes in an array, swaps two items, and returns a new array. Returns the same array if swapping causes an item to go beyond the array. */
+export function swapArrayItems<T>(array: T[], a: number, b: number): T[] {
+    if (a < 0 || b < 0) return array;
+    if (a >= array.length || b >= array.length) return array;
+
+    const newArray = array.slice();
+    [newArray[a], newArray[b]] = [newArray[b], newArray[a]];
+    return newArray;
+}
