fd1068ac49a54b1c17df89e7fbf3b69916cc7d6b
caasion <103465188+caasion@users.noreply.github.com>
2025-10-16 22:25:05 -0400

    [feat] Implement All Services

--- src/actions/calendarPipelines.ts
+++ src/actions/calendarPipelines.ts
@@ -1,9 +1,10 @@
 import { Notice } from "obsidian";
-=import type { CalendarHelperService, CalendarID, CalendarMeta, CalendarStatus, DataService, HelperService, NormalizedEvent } from "../types";
+import type { CalendarHelperService, CalendarID, CalendarMeta, CalendarStatus, DataService, FetchService, HelperService, NormalizedEvent } from "../types";
 import { get } from "svelte/store";
 
 export interface CalendarServiceDeps {
     data: DataService;
+    fetch: FetchService;
     helpers: HelperService;
     calHelpers: CalendarHelperService;
 }
@@ -11,12 +12,14 @@ export interface CalendarServiceDeps {
 export class CalendarPipeline {
     // Declare properties to hold injected dependencies
     private data: DataService;
+    private fetch: FetchService;
     private helpers: HelperService;
     private calHelpers: CalendarHelperService;
 
     // Inject dependencies via constructor
     constructor(deps: CalendarServiceDeps) {
         this.data = deps.data;
+        this.fetch = deps.fetch;
         this.helpers = deps.helpers;
         this.calHelpers = deps.calHelpers;
     }
@@ -24,6 +27,10 @@ export class CalendarPipeline {
     /** Helper to access calendar status. */
     private setCalendarStatus(status: CalendarStatus) {
         this.data.calendarState.set({ status })
+    }
+
+    private setCalendarError(error: string) {
+        this.data.calendarState.set({ status: "error", lastError: error })
     }
 
     private getCalendarStatus(): CalendarStatus {
@@ -56,7 +63,7 @@ export class CalendarPipeline {
         const startUrl = calendar.url;
 
         try { // Wrap in try because fetchFromUrl throws Exception
-            const response = await fetchFromUrl(calendar.url, calendar.etag, calendar.lastModified); 	
+            const response = await this.fetch.fetchFromUrl(calendar.url, calendar.etag, calendar.lastModified); 	
 
             // [STORE] Update lastFetched status in store
             this.data.updateItem("2025-10-16" /* Placeholder date */, calendar.id, { lastFetched: Date.now() })
@@ -81,8 +88,8 @@ export class CalendarPipeline {
 
             // [CONDITION] If the calendar contents didn't change, don't bother updating freeze and cache.
             const contentHash = await this.helpers.hashText(JSON.stringify(events));
-            if (!detectFetchChange(response, contentHash, calendar.contentHash)) {
-                setCalendarStatus("unchanged");
+            if (!this.fetch.detectFetchChange(response, contentHash, calendar.contentHash)) {
+                this.setCalendarStatus("unchanged");
                 return;
             }
             
@@ -90,132 +97,15 @@ export class CalendarPipeline {
             this.data.updateItem("2025-10-16", calendar.id, { contentHash })
             
             // [STORE] Build efficient event dictionaries and use those to write into store
-            const { index, eventsById } = buildEventDictionaries(events);
-            populateCalendarCells(calendar.id, index, eventsById);
+            const { index, eventsById } = this.calHelpers.buildEventDictionaries(events);
+            this.populateCells(calendar.id, index, eventsById);
 
-
-            setCalendarStatus("updated");
+            this.setCalendarStatus("updated");
             
         } catch (error) {
-            calendarState.update(() => { return { status: "error", lastError: error } });
+            this.setCalendarError(error);
             new Notice("An error occured while fetching. See console for details");
             console.error("An error occured while fetching:", error.message)
         }
-
-
-    }
-}
-
-export async function fetchPipelineInGracePeriod(calendar: CalendarMeta, after: Date, before: Date) {
-    // Check if we should fetch. If we do fetch, set status.
-    if (get(calendarState).status === "fetching") return; 
-    setCalendarStatus("fetching");
-
-    // [SETUP] fetchToken for GUARD later
-    fetchToken.update(token => token + 1)
-    const myToken = get(fetchToken);
-
-    // [SETUP] startUrl for GUARD later
-    const startUrl = calendar.url;
-
-    try { // Wrap in try because fetchFromUrl throws Exception
-        const response = await fetchFromUrl(calendar.url, calendar.etag, calendar.lastModified); 	
-
-        // [STORE] Update lastFetched status in store
-        updateCalendar(calendar.id, { lastFetched: Date.now() });
-
-        // [GUARD] If a new refresh token is generated, that means our fetch is stale (old data). We want to drop that.
-        if (myToken !== get(fetchToken)) {
-            console.warn("Fetch request is stale. Aborted.");
-            setCalendarStatus("unchanged");
-            return;
-        };
-
-        // [GUARD] If the old URL is different from the current one, the URL changed and we should drop the fetch
-        if (startUrl !== calendar.url) { // We can do this because calendar is a reference to the object
-            new Notice("URL changed during fetch. Please fetch again.");
-            console.warn("URL changed during fetch. Aborted.");
-            setCalendarStatus("unchanged");
-            return;
-        };
-
-        // [PARSE] Parse the ICS within the grace period
-        const events = parseICSBetween(response.text, this._defaultCalendar, after, before);
-
-        // [CONDITION] If the calendar contents didn't change, don't bother updating freeze and cache.
-        const contentHash = await hashText(JSON.stringify(events));
-        if (!detectFetchChange(response, contentHash, calendar.contentHash)) {
-            setCalendarStatus("unchanged");
-            return;
-        }
-        
-        // [STORE] Update cache information 
-        updateCalendar(calendar.id, { contentHash })
-        
-        // [STORE] Build efficient event dictionaries and use those to write into store
-        const { index, eventsById } = buildEventDictionaries(events);
-        populateCalendarCells(calendar.id, index, eventsById);
-
-
-        setCalendarStatus("updated");
-        
-    } catch (error) {
-        calendarState.update(() => { return { status: "error", lastError: error } });
-        new Notice("An error occured while fetching. See console for details");
-        console.error("An error occured while fetching:", error.message)
-    }
-}
-
-export async function fetchAllandFreeze(calendar: CalendarMeta, after: Date, before: Date) {
-    // Check if we should fetch. If we do fetch, set status.
-    if (get(calendarState).status === "fetching") return; 
-    setCalendarStatus("fetching");
-
-    // [SETUP] fetchToken for GUARD later
-    fetchToken.update(token => token + 1)
-    const myToken = get(fetchToken);
-
-    // [SETUP] startUrl for GUARD later
-    const startUrl = calendar.url;
-
-    try { // Wrap in try because fetchFromUrl throws Exception
-        const response = await fetchFromUrl(calendar.url, calendar.etag, calendar.lastModified); 	
-
-        // [STORE] Update lastFetched status in store
-        updateCalendar(calendar.id, { lastFetched: Date.now() })
-
-        // [GUARD] If a new refresh token is generated, that means our fetch is stale (old data). We want to drop that.
-        if (myToken !== get(fetchToken)) {
-            console.warn("Fetch request is stale. Aborted.");
-            setCalendarStatus("unchanged");
-            return;
-        };
-
-        // [GUARD] If the old URL is different from the current one, the URL changed and we should drop the fetch
-        if (startUrl !== calendar.url) { // We can do this because calendar is a reference to the object
-            new Notice("URL changed during fetch. Please fetch again.");
-            console.warn("URL changed during fetch. Aborted.");
-            setCalendarStatus("unchanged");
-            return;
-        };
-        
-        // [FREEZE] Parse ALL events, build dictionaries, and freeze
-        const allEvents = parseICS(response.text, this._defaultCalendar);
-        const { index, eventsById } = buildEventDictionaries(allEvents);
-        populateCalendarCells(calendar.id, index, eventsById);
-        
-        // [HASH] Parse ICS within grace period and compute contentHash from it
-        const eventsBetween = parseICSBetween(response.text, this._defaultCalendar, after, before);
-        const contentHash = await hashText(JSON.stringify(eventsBetween));
-
-        // [STORE] Update cache information 
-        updateCalendar(calendar.id, { contentHash })
-
-        setCalendarStatus("updated");
-        
-    } catch (error) {
-        calendarState.update(() => { return { status: "error", lastError: error } });
-        new Notice("An error occured while fetching. See console for details");
-        console.error("An error occured while fetching:", error.message)
     }
 }
