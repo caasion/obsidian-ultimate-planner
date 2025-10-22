469c272c113a295b8620c4ea5227d789787cc626
caasion <103465188+caasion@users.noreply.github.com>
2025-10-22 09:11:01 -0400

    [feat] Templates Editor UI

---
+++ src/ui/TemplateEditor.svelte
@@ -0,0 +1,52 @@
+<div class="container">
+    <div class="section">
+        <div class="header">
+            <h2>Templates</h2>
+            <button>+ Add</button>
+        </div>
+        <div class="templates-container">
+            
+        </div>
+
+    </div>
+    <div class="section">
+        <h2>Template yyyy-MM-dd</h2>
+        This is another div
+        <div class="item-container">
+            <div class="item">
+                Item!
+            </div>
+        </div>
+    </div>
+</div>
+
+<style>
+    .container {
+        margin: 5%;
+        display: flex;
+    }
+
+    .section {
+        border: 2px solid #acacac;
+        border-style: solid;
+        border-radius: 4px;
+        width: 100%;
+        padding: 10px;
+        margin: 0px 5px;
+    }
+
+    .header {
+        display: flex;
+        justify-content: space-between;
+        align-items: baseline;
+    }
+
+    .item {
+        border: 2px solid #acacac;
+        border-style: solid;
+        border-radius: 4px;
+        width: 100%;
+        padding: 5px;
+        margin: 5px 0px;
+    }
+</style>
