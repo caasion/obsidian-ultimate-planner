d8dcf89461080f4e04951af6d245f0b827e96028
Isaac (Tsz Lai) Ng <103465188+caasion@users.noreply.github.com>
2026-06-02 11:57:25 -0400

    adjust: GanttHeader styling

--- src/gantt/ui/GanttHeader.svelte
+++ src/gantt/ui/GanttHeader.svelte
@@ -26,7 +26,8 @@
   .gantt-header {
     position: relative;
     height: 32px;
-    border-bottom: 1px solid var(--background-modifier-border);
+    background: rgba(255, 255, 255, 0.03);
+    border-bottom: 1px solid rgba(255, 255, 255, 0.1);
     flex-shrink: 0;
   }
 
